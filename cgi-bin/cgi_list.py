4a8e546449cb4e60a24c91a61fd4904764d2b898
Mendel Cyprys <mmcyprys@gmail.com>
2024-03-04 11:42:26 +0000

    Stuff

---
+++ cgi-bin/cgi_list.py
@@ -0,0 +1,16 @@
+#!C:\Python\Python3.1.2\python.exe
+
+# use the Python path in use, `where python` in cmd to find
+
+# this has to be outputted first
+# it is a header that tells the browser that this file is in HTML form
+print("Content-type: text/html")
+
+# after the headers we print a new line, to separate the headers from the content
+print("")
+
+
+print("<html><head><title>List from file</title></head><body>")
+print("<h1>Hello from Python CGI!</h1>")
+print("<p>This is a basic CGI script written in Python running on a Windows environment.</p>")
+print("</body></html>")
